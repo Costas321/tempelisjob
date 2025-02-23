098acf81751075e893026de659f137175561dacc
Costas321 <152185183+Costas321@users.noreply.github.com>
2025-02-23 19:17:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.querySelector(".img2").setAttribute("src", "images/sport24_logo.png");
   
-    document.querySelector("h2").innerHTML = "Μία δουλίτσα να γλείφω ό,τι κινείται";
+    document.querySelector("h2").innerHTML = "Μια δουλίτσα να γλείφω ό,τι κινείται";
   
     document.querySelector(".change-image-btn").addEventListener("click", function() {
       let randomImageNum = Math.floor(Math.random() * 17) + 1;
